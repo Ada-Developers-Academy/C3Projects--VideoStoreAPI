acebf38bf68c27448297c3a1f14af4c1b81570a1
Lila Garcia <lilagrc@gmail.com>
2015-09-15 16:35:36 -0700

    Changed route names to movies and customers. Added controllers

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+var routes = require('./routes/movies');
+app.use('/movies', routes);
+
+var customers = require('./routes/customers');
+app.use('/customers', customers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
