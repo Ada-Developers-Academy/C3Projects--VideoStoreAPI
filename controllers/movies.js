7db7a9329ce67b05de957b577dec76ab91c200f1
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-15 16:58:27 -0700

    Added pseudo-code for movies.

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,4 +7,20 @@ exports.moviesController = {
     }
     return res.status(200).json(results);
   }
+
+  /*
+  GET /movies
+
+  GET /movies/title?n=XXX&p=XXX
+
+  GET /movies/release_date?n=XXX&p=XXX
+
+  GET /movies/:title/checked_out_current
+
+  GET /movies/:title/checked_out_history?ordered_by=XXX
+    // ordered_by
+      // - id
+      // - name
+      // - check out date
+  */
 }
