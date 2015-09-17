369b3f4eb58c7d0bb088781f90d9113dd2ec77c8
Kyna <khn710@gmail.com>
2015-09-17 15:58:58 -0700

    inital route and controller setup for movies

---
+++ controllers/movies.js
@@ -0,0 +1,55 @@
+"use strict";
+
+exports.moviesController = {
+  index: function index(req,res) {
+    var results = {
+      // all movies
+    }
+  return res.status(200).json(results);
+  },
+
+  title: function title(req,res) {
+    var results = {
+      // all movies sorted by title
+    }
+  return res.status(200).json(results);
+  },
+
+  released: function released(req,res) {
+    var results = {
+      // all movies by release date
+    }
+  return res.status(200).json(results);
+  },
+
+  checkedout: function checkedout(req,res) {
+    var results = {
+      // for a movie, all customers w/ it checked out
+    }
+  return res.status(200).json(results);
+  },
+
+  title_history: function title_history(req,res) {
+    var results = {
+      // for a movie, all customers w/ that movie previously checked out
+      // sorted by check out date
+    }
+  return res.status(200).json(results);
+  },
+
+  id_history: function id_history(req,res) {
+    var results = {
+      // for a movie, all customers w/ that movie previously checked out
+      // sorted by
+    }
+  return res.status(200).json(results);
+  },
+
+  name_history: function name_history(req,res) {
+    var results = {
+      // for a movie, all customers w/ that movie previously checked out
+      // sorted by
+    }
+  return res.status(200).json(results);
+  }
+}
--- routes/movies.js
+++ routes/movies.js
@@ -6,5 +6,33 @@ var movies_exports = require('../controllers/movies')
 router.get('/', function(req, res, next) {
   return movies_exports.moviesController.index(req, res);
 });
+// sort by movie title
+router.get('/title/:query', function(req, res, next) {
+  return movies_exports.moviesController.title(req, res);
+});
+
+// sort by movie release date
+router.get('/released/:query', function(req, res, next) {
+  return movies_exports.moviesController.released(req, res);
+});
+
+// for specific movie, show all customers w/ it checkout out
+router.get('checkedout/:title', function(req, res, next) {
+  return movies_exports.moviesController.checkedout(req, res);
+});
+// all customers w/ specific movie checked out in past
+// default show sorted by check out date??
+router.get('/history/:title', function(req, res, next) {
+  return movies_exports.moviesController.title_history(req, res);
+});
+// for movie, sort check out history by customer id
+router.get('/history/id/:title', function(req, res, next) {
+  return movies_exports.moviesController.id_history(req, res);
+});
+// for movie, sort check out history by customer name
+router.get('/history/name/:title', function(req, res, next) {
+  return movies_exports.moviesController.name_history(req, res);
+});
+
 
 module.exports = router;
