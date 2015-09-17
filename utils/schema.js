d83cf6e2353ccf646cfbaf04ed68910f78fe4496
Kyna <khn710@gmail.com>
2015-09-17 14:37:18 -0700

    seeded all data

--- utils/schema.js
+++ utils/schema.js
@@ -11,12 +11,36 @@ var sqlite3 = require('sqlite3').verbose(),
     ['inventory', 'integer']
   ]
 
+  var customer_fields = [
+    ['name', 'text'],
+    ['registered_at', 'text'],
+    ['address', 'text'],
+    ['city', 'text'],
+    ['state', 'text'],
+    ['postal_code', 'text'],
+    ['phone', 'text'],
+    ['account_credit', 'real']
+  ]
+
+  var rental_fields = [
+    ['checkout_date', 'text'],
+    ['due_date', 'text'],
+    ['return_date', 'text'],
+    ['overdue', 'integer'],
+    ['movie_id', 'integer'],
+    ['customer_id', 'integer']
+  ]
+
   db.serialize(function() {
     //drop existing
     db.run("DROP TABLE IF EXISTS movies;");
-    // db.run("DROP TABLE IF EXISTS customers;")
+    db.run("DROP TABLE IF EXISTS rentals;");
+    db.run("DROP TABLE IF EXISTS customers;");
+
     //create tables
     db.run("CREATE TABLE movies (id INTEGER PRIMARY KEY);");
+    db.run("CREATE TABLE rentals (id INTEGER PRIMARY KEY);");
+    db.run("CREATE TABLE customers (id INTEGER PRIMARY KEY);");
 
     //add columns
     for(var i = 0; i < movie_fields.length; i++) {
@@ -24,5 +48,18 @@ var sqlite3 = require('sqlite3').verbose(),
           type = movie_fields[i][1];
       db.run("ALTER TABLE movies ADD COLUMN " + name + " " + type + ";");
     }
+
+    for(var j = 0; j < rental_fields.length; j++) {
+      var name = rental_fields[j][0],
+          type = rental_fields[j][1];
+      db.run("ALTER TABLE rentals ADD COLUMN " + name + " " + type + ";");
+    }
+
+    for(var k = 0; k < customer_fields.length; k++) {
+      var name = customer_fields[k][0],
+          type = customer_fields[k][1];
+      db.run("ALTER TABLE customers ADD COLUMN " + name + " " + type + ";");
+    }
   });
+  
   db.close();
--- utils/seed.js
+++ utils/seed.js
@@ -10,6 +10,19 @@ var sqlite3 = require('sqlite3').verbose(),
     VALUES (?, ?, ?, ?);"
   );
 
+  var customers = require('./customers');
+  var customers_statement = db.prepare(
+    "INSERT INTO customers(name, registered_at, address, city, state, postal_code, phone, account_credit) \
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
+  );
+
+  var rentals = require('./rentals');
+  var rentals_statement = db.prepare(
+    "INSERT INTO rentals(checkout_date, due_date, return_date, overdue, movie_id, customer_id) \
+    VALUES (?, ?, ?, ?, ?, ?);"
+  );
+
+  //loop through movies
   db.serialize(function() {
     for(var i = 0; i < movies.length; i++) {
       var movie = movies[i];
@@ -20,8 +33,36 @@ var sqlite3 = require('sqlite3').verbose(),
         movie.inventory
       );
     }
+    movie_statement.finalize();
+
+    for(var j = 0; j < rentals.length; j++) {
+      var rental = rentals[j];
+      rentals_statement.run(
+        rental.checkout_date,
+        rental.due_date,
+        rental.return_date,
+        rental.overdue,
+        rental.movie_id,
+        rental.customer_id
+      );
+    }
+    rentals_statement.finalize();
+
+    for(var k = 0; k < customers.length; k++) {
+      var customer = customers[k];
+      customers_statement.run(
+        customer.name,
+        customer.registered_at,
+        customer.address,
+        customer.city,
+        customer.state,
+        customer.postal_code,
+        customer.phone,
+        customer.account_credit
+      );
+    }
+    customers_statement.finalize();
   })
-  //loop through movies
-  //insert each into db
+
 
 db.close();
