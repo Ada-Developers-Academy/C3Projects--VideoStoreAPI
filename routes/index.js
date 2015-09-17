a2213ff2a82dc5107762f597af07c90290355ef5
Anita <asovay@gmail.com>
2015-09-17 14:37:53 -0700

    removed users routes and references and added /zomg route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/zomg', function(req, res, next) {
+  return res.status(200).json({ message: "it works!" });
+});
+
 module.exports = router;
