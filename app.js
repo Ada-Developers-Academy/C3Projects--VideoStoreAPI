81efc00890f02d97d88b61b55d3505f409a9c4d0
Kari <kari.lonczak@gmail.com>
2015-09-17 13:35:17 -0700

    Added more boilerplate directories for db and utils. Moved seeds into utils folder

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,7 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var routes = require('./routes/index');
 app.use('/', routes);
+
+var users = require('./routes/users');
 app.use('/users', users);
 
 // catch 404 and forward to error handler
