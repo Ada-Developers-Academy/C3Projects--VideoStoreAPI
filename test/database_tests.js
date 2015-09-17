ce0e282841f25c305ee25e217cae1a5a1c7c83d6
Kari <kari.lonczak@gmail.com>
2015-09-17 14:45:46 -0700

    Wrote tests for database.js file

---
+++ test/database_tests.js
@@ -0,0 +1,41 @@
+var assert = require("assert");
+var Database = require("../database"); // our database.js file
+
+describe("Database", function() {
+  var db;
+  var database_path = "db/test.db";
+
+  beforeEach(function() {
+    db = new Database(database_path);
+  });
+
+  it("can be instantiated", function() {
+    assert.equal(db instanceof Database, true);
+  });
+
+  it("has a 'query' property that is a function", function() {
+    assert.equal(typeof db.query, "function");
+  });
+
+  it("holds onto the 'path' to the database", function() {
+    assert.equal(db.path, database_path);
+  });
+
+  describe("'query' function", function() {
+    before(function() {
+      // create a customers table
+      db.query("CREATE TABLE IF NOT EXISTS customers (name TEXT);");
+
+      // insert some customers
+    });
+
+    it("has a customers table", function(done) {
+      var table_exists = "SELECT count(*) AS table_count FROM sqlite_master WHERE type='table' AND name='customers'; ";
+
+      db.query(table_exists, function(result) {
+        assert.equal(result[0].table_count, 1);
+        done();
+      });
+    });
+  });
+});
