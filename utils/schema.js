34db91529b389359aec407f442f3e9559b434ea8
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-17 14:37:20 -0700

    Added schema for all tables & added seed data from json files.

--- utils/schema.js
+++ utils/schema.js
@@ -28,4 +28,57 @@ db.serialize(function() {
   }
 });
 
+var customer_fields = [
+  ['name', 'text'],
+  ['registered_at', 'text'],
+  ['address', 'text'],
+  ['city', 'text'],
+  ['state', 'text'],
+  ['postal_code', 'text'],
+  ['phone', 'text'],
+  ['account_credit', 'real']
+]
+
+db.serialize(function() {
+  // drop existing tables
+  db.run("DROP TABLE IF EXISTS customers;");
+
+  // create fresh versions of those tables
+  db.run("CREATE TABLE customers (id INTEGER PRIMARY KEY);");
+
+  // add columns that I need to those tables
+  for(var i = 0; i < customer_fields.length; i++) {
+    var name = customer_fields[i][0],
+        type = customer_fields[i][1];
+
+    // ALTER TABLE movies ADD COLUMN title text;
+    db.run("ALTER TABLE customers ADD COLUMN " + name + " " + type + ";");
+  }
+});
+
+var rental_fields = [
+  ['check_out_date', 'text'],
+  ['check_in_date', 'text'],
+  ['expected_return_date', 'text'],
+  ['customer_id', 'integer'],
+  ['movie_id', 'integer']
+]
+
+db.serialize(function() {
+  // drop existing tables
+  db.run("DROP TABLE IF EXISTS rentals;");
+
+  // create fresh versions of those tables
+  db.run("CREATE TABLE rentals (id INTEGER PRIMARY KEY);");
+
+  // add columns that I need to those tables
+  for(var i = 0; i < customer_fields.length; i++) {
+    var name = customer_fields[i][0],
+        type = customer_fields[i][1];
+
+    // ALTER TABLE movies ADD COLUMN title text;
+    db.run("ALTER TABLE rentals ADD COLUMN " + name + " " + type + ";");
+  }
+});
+
 db.close();
--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,58 @@
 "use strict";
+
+var sqlite3 = require('sqlite3').verbose(),
+    db_env = process.env.DB || 'development',
+    db = new sqlite3.Database('db/' + db_env + '.db');
+
+var movies = require('./movies');
+//insert into movies(title, inventory) values("Jaws", 10);
+var movie_statement = db.prepare(
+  "INSERT INTO movies(title, overview, inventory, release_date) \
+  VALUES (?, ?, ?, ?);"
+);
+
+db.serialize(function() {
+  // loop them movies
+  for(var i = 0; i < movies.length; i++) {
+    var movie = movies[i];
+
+    // insert each one into the db
+    movie_statement.run(
+      movie.title,
+      movie.overview,
+      movie.inventory,
+      movie.release_date
+    );
+  }
+
+  movie_statement.finalize();
+});
+
+var customers = require('./customers');
+var customer_statement = db.prepare(
+  "INSERT INTO customers(name, registered_at, address, city, state, postal_code, \
+  phone, account_credit) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
+);
+
+db.serialize(function() {
+  // loop them movies
+  for(var i = 0; i < customers.length; i++) {
+    var customer = customers[i];
+
+    // insert each one into the db
+    customer_statement.run(
+      customer.name,
+      customer.registered_at,
+      customer.address,
+      customer.city,
+      customer.state,
+      customer.postal_code,
+      customer.phone,
+      customer.account_credit
+    );
+  }
+
+  customer_statement.finalize();
+});
+
+db.close();
