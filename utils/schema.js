c07986737eaa71a36ed33de1d9025ae0c41eb47e
Carly Jugler <carjug@gmail.com>
2015-09-17 14:30:03 -0700

    Adds customers schema to schema and development.db

--- utils/schema.js
+++ utils/schema.js
@@ -9,24 +9,44 @@ var movie_fields = [
   ['overview', 'text'],
   ['release_date', 'text'],
   ['inventory', 'integer']
-]
+];
+
+var customer_fields = [
+  ['name', 'text'],
+  ['registered_at', 'text'],
+  ['address', 'text'],
+  ['city', 'text'],
+  ['state', 'text'],
+  ['postal_code', 'text'],
+  ['phone', 'text'],
+  ['account_credit', 'integer']
+];
 
 db.serialize(function() {
   // drop existing tables
   db.run("DROP TABLE IF EXISTS movies;");
+  db.run("DROP TABLE IF EXISTS customers;");
 
   // create fresh versions of those tables
   db.run("CREATE TABLE movies (id INTEGER PRIMARY KEY);");
-
+  db.run("CREATE TABLE customers (id INTEGER PRIMARY KEY);");
   // add columns that I need to those tables
   for(var i = 0; i < movie_fields.length; i++) {
     var name = movie_fields[i][0],
         type = movie_fields[i][1];
-        console.log(name);
-        console.log(type);
+
     // ALTER TABLE movies ADD COLUMN title text;
     db.run("ALTER TABLE movies ADD COLUMN " + name + " " + type + ";");
   }
+
+   for(var i = 0; i < customer_fields.length; i++) {
+    var name = customer_fields[i][0],
+        type = customer_fields[i][1];
+
+    // ALTER TABLE customers ADD COLUMN title text;
+    db.run("ALTER TABLE customers ADD COLUMN " + name + " " + type + ";");
+  }
+
 });
 
 db.close();
