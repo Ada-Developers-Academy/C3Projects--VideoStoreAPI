e391ee74658aeb55a962e41e3107affa48f0289a
Kari <kari.lonczak@gmail.com>
2015-09-18 15:16:04 -0700

    Created movies.js controller file with stubbed out functions.

---
+++ controllers/movies.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var Database = require('../database'); // Node module; Pulling in db object we made in a dif file
+
+var moviesController = {
+  // maybe move the var db = ... out here?
+  all_movies: function() {
+
+  },
+  movie: function() {
+    // Amira
+  }
+};
+
+module.exports = moviesController;
