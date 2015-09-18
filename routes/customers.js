47fdf74a239a076af92e6774bb38bf06763199d8
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-18 11:42:50 -0700

    added debugger route to test  function of database-adapter

--- routes/customers.js
+++ routes/customers.js
@@ -18,4 +18,17 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
+// DEBUGGER ROUTE
+
+// router.get('/:city', function(req, res, next) {
+//   var values = [];
+//   values.push(req.params.city);
+//   console.log(values);
+
+//   customer.where(['city'], values, function(err, rows) {
+//     console.log(values);
+//     res.status(200).json({ customers: rows} );
+//   });
+// });
+
 module.exports = router;
