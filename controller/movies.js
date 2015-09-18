0a33ce71b1605653fd489a944ffa5ddb768406d6
Shanna Chau <shannachau@gmail.com>
2015-09-18 11:42:55 -0700

    Added sort_pages function. Sorts movies by title or release_date. WIP for pagination

--- controller/movies.js
+++ controller/movies.js
@@ -5,9 +5,27 @@ var db_env = process.env.DB || 'development';
 var db = new sqlite3.Database('db/' + db_env + '.db');
 
 exports.moviesController = {
+  // '/movies'
   all_movies: function(req, res) {
     db.all("SELECT * FROM movies;", function(err, rows) {
       res.status(200).json(rows);
     });
+  },
+
+  // '/movies/:sort_by/:results_per_page/:page_number'
+  sort_pages: function(req, res) {
+    if (req.params.sort_by == "title") {
+      db.all("SELECT * FROM movies ORDER BY title ASC;", function(err, rows) {
+        res.status(200).json(rows);
+      });
+    }
+
+    if (req.params.sort_by == "release_date") {
+      console.log(req.params.sort_by);
+      db.all("SELECT * FROM movies ORDER BY release_date ASC;", function(err, rows) {
+        res.status(200).json(rows);
+      });
+    }
+
   }
 }
--- routes/movies.js
+++ routes/movies.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 var movies_exports = require('../controller/movies');
 
-// route is '/movies'
+// '/movies'
 router.get('/', function(req, res, next) {
   return movies_exports.moviesController.all_movies(req, res);
+});
+
+// '/movies/:sort_by/:results_per_page/:page_number'
+router.get('/:sort_by/:results_per_page/:page_number', function(req, res, next) {
+  return movies_exports.moviesController.sort_pages(req, res);
 });
 
 module.exports = router;
