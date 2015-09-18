4f327d2781ec1693e99d310a9b56613b477c2e91
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-18 15:45:09 -0700

    removed line used for testing

--- models/movies.js
+++ models/movies.js
@@ -3,9 +3,8 @@
 var sqlite3 = require('sqlite3').verbose();
 
 function Movie() {
-  this.test = 'Test';
   this.tableName = "movies";
-};
+}
 
 // this is silly-ish, but necessary because of how we set up the DB object
 Movie.prototype = require('./database').prototype;
