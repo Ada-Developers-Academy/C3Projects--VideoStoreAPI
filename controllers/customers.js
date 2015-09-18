7595884befb83f7db87201af1fde306dc5e6aa2e
Carly Jugler <carjug@gmail.com>
2015-09-18 11:00:33 -0700

    Adds WIP customers by name route and function

--- controllers/customers.js
+++ controllers/customers.js
@@ -4,7 +4,6 @@ var sqlite3 = require('sqlite3').verbose(),
     db_env  = process.env.DB || 'development',
     db;
 
-
 exports.customersController = {
   customers: function(req, res) {
     db = new sqlite3.Database('db/' + db_env + '.db');
@@ -12,5 +11,16 @@ exports.customersController = {
       db.close();
       return res.status(200).json(all_customers);
     });
-  }
+  },
+
+  customers_by_name: function(req, res) {
+    db = new sqlite3.Database('db/' + db_env + '.db');
+    var name = req.params.name.toLowerCase();
+    db.get("SELECT * FROM customers WHERE name LIKE %?%;", name, function(err, the_name) {
+      db.close();
+      return res.status(200).json(the_name);
+    });
+
+  },
+
 };
--- routes/customers.js
+++ routes/customers.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   return customer_exports.customersController.customers(req, res);
 });
 
+router.get('/names/:name', function(req, res, next) {
+  return customer_exports.customersController.customers_by_name(req, res);
+});
+
 module.exports = router;
