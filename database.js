291c4b956db539bf629c6b9367091bd7551ea0a7
Katie Atrops <katrops@gmail.com>
2015-09-18 13:11:53 -0700

    set up database tests and stuff

--- database.js
+++ database.js
@@ -3,12 +3,46 @@
 var sqlite3 = require("sqlite3").verbose();
   db_env = process.env.DB || 'development';
 
-function Database(path) {
-  this.path = path
+module.exports = {
+  find_by: function(column, value, callback) {
+    var db = new sqlite3.Database('db/' + db_env + '.db');
+    var statement = "SELECT * FROM " + this.table_name + " WHERE " + column + " = ?;";
+
+    db.all(statement, value, function(err, res) {
+      if (callback) callback(err, res);
+      db.close();
+    });
+  }
+
+  create_rental: function(data, callback) {
+    var db = new sqlite3.Database('db/' + db_env + '.db');
+    var statement = "INSERT INTO rentals (checkout_date, returned_date, rental_time, \
+                    cost, total, customer_id, movie_id) \
+                    VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+    var values = []
+    values.push(data.checkout_date);
+    values.push(data.returned_date);
+    values.push(data.rental_time);
+    values.push(data.cost);
+    values.push(data.total);
+    values.push(data.customer_id);
+    values.push(data.movie_id);
+
+
+    db.run(statement, values, function(err) {
+      callback(err, res);
+      db.close();
+    });
+  }
 }
 
-Database.prototype = {
-  
-}
+// function Database(path) {
+//   this.path = path
+// }
 
-module.exports = Database;
+// Database.prototype = {
+
+// }
+
+// module.exports = Database;
