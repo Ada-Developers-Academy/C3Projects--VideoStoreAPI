bbc4a06aa3585e7712553485208ef8f84f101a7e
Loraine <loraine@lorainekv.com>
2015-09-18 14:23:20 -0700

    Changed variables names so we wouldn't get confused about res.

--- database.js
+++ database.js
@@ -8,8 +8,8 @@ module.exports = {
     var db = new sqlite3.Database('db/' + db_env + '.db');
     var statement = "SELECT * FROM " + this.table_name + ";";
 
-     db.all(statement, function(err, res) {
-      if (callback) callback(err, res);
+     db.all(statement, function(err, record) {
+      if (callback) callback(err, record);
       db.close();
     });
   },
