a2661f8040fc1e69651f8b66f015f069d4c8e0c7
Elsa <elsa.moluf@gmail.com>
2015-09-18 10:00:01 -0700

    Cleaned up console logs calls.

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var sqlite3 = require('sqlite3').verbose(),
-    db_env = process.env.DB || 'development',
-    db     = new sqlite3.Database('db/' + db_env + '.db');
+    db_env  = process.env.DB || 'development',
+    db      = new sqlite3.Database('db/' + db_env + '.db');
 
 exports.moviesController = {
   movies: function(req, res) {
@@ -11,23 +11,10 @@ exports.moviesController = {
     });
   },
 
-  // movies_by_title: function(req, res) {
-  //   var title = req.params.title.toLowerCase();
-  //   console.log(title);
-  //   db.get("SELECT * FROM movies WHERE title LIKE '" + title + "';", function(err, the_title) {
-  //     console.log(typeof title);
-  //     return res.status(200).json(the_title);
-  //
-  //   });
-  // }
-
   movies_by_title: function(req, res) {
     var title = req.params.title.toLowerCase();
-    console.log(title);
     db.get("SELECT * FROM movies WHERE title LIKE ?;", title, function(err, the_title) {
-      console.log(typeof title);
       return res.status(200).json(the_title);
-
     });
   }
 
