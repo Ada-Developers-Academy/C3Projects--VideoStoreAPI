e9cd9053e2019d08cd98725287f72ba4dc684928
Carly Jugler <carjug@gmail.com>
2015-09-18 10:37:17 -0700

    Adds route for movie by release date

--- controllers/movies.js
+++ controllers/movies.js
@@ -23,10 +23,16 @@ exports.moviesController = {
       return res.status(200).json(the_title);
     });
 
+  },
+
+  movies_by_release: function(req, res) {
+    db = new sqlite3.Database('db/' + db_env + '.db');
+    var release = req.params.release_date;
+    db.get("SELECT * FROM movies WHERE release_date=?;", release, function(err, the_date) {
+      db.close();
+      return res.status(200).json(the_date);
+    });
+
   }
-
-
-
-
 
 }; // end exports.moviesController
--- routes/movies.js
+++ routes/movies.js
@@ -14,4 +14,9 @@ router.get('/:title', function(req, res, next) {
   return movies_exports.moviesController.movies_by_title(req, res);
 });
 
+router.get('/release/:release_date', function(req, res, next) {
+  // res.render('movies', { title: 'Express' });
+  return movies_exports.moviesController.movies_by_release(req, res);
+});
+
 module.exports = router;
