8f45370377e67483a60c3928ea5f16ae5b02c9bd
Lila Garcia <lilagrc@gmail.com>
2015-09-18 11:26:08 -0700

    Added customer model. Added database.js file. Added first model test.

---
+++ database.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var sqlite3 = require('sqlite3').verbose(),
+  db_env = process.env.DB || 'development';
+
+module.exports = {
+  find_by: function(column, value, callback) {
+    var db = new sqlite3.Database('db/' + db_env + '.db');
+    var statement = "SELECT * FROM " + this.table_name + " WHERE " + column + "= ?;";
+
+    db.all(statement, value, function(err, res) {
+      if (callback) callback(err, res);
+      db.close();
+    });
+  },
+
+
+
+
+}
--- routes/customers.js
+++ routes/customers.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var customer_exports = require('../controllers/customers');
+var customer_exports = require('../models/customers');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
