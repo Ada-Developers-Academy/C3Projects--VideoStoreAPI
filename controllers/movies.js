da3013e35df3775627172788df42285942de6138
Sally Moore <sallyamoore@gmail.com>
2015-09-18 14:53:43 -0700

    Set up schema in test.db and wrote test for /movies endpoint.

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,5 @@
 "use strict";
-var Movie = require('../models/movies'); //class needs to be instantiated
+var Movie = require('../models/movie'); //class needs to be instantiated
 
 exports.moviesController = {
   database_test: function(req, res) {
@@ -10,7 +10,7 @@ exports.moviesController = {
   },
 
   index: function(req, res) {
-    var db = new Movie(); 
+    var db = new Movie();
     db.find_all(function(err, result) {
 
       return res.status(200).json(result);
