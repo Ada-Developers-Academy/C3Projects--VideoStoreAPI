01d2fe386b4245f11644ea1c15cc9cebe6bf09cc
Elsa <elsa.moluf@gmail.com>
2015-09-18 10:14:39 -0700

    Closing db after requests now.

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,21 +2,30 @@
 
 var sqlite3 = require('sqlite3').verbose(),
     db_env  = process.env.DB || 'development',
-    db      = new sqlite3.Database('db/' + db_env + '.db');
+    db;
+
 
 exports.moviesController = {
   movies: function(req, res) {
+    db = new sqlite3.Database('db/' + db_env + '.db');
     db.all("SELECT * FROM movies", function(err, all_movies) {
+      db.close();
       return res.status(200).json(all_movies);
     });
+
   },
 
   movies_by_title: function(req, res) {
+    db = new sqlite3.Database('db/' + db_env + '.db');
     var title = req.params.title.toLowerCase();
     db.get("SELECT * FROM movies WHERE title LIKE ?;", title, function(err, the_title) {
+      db.close();
       return res.status(200).json(the_title);
     });
+
   }
+
+
 
 
 
