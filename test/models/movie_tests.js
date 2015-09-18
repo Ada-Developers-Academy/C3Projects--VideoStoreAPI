4793b2b87a021d391f45682791a413d473eaf1df
Victoria <vikshab2010@hotmail.com>
2015-09-18 15:35:08 -0700

    Created schema for test Wrote test for find_by function We have 3 green tests We added "test_schema": "DB=test node ./utils/schema.js" to package.json

--- test/models/movie_tests.js
+++ test/models/movie_tests.js
@@ -1,10 +1,48 @@
-var assert = require("assert");
-var Movie = require('../../models/movie');
+var assert = require("assert"),
+    Movie = require('../../models/movie'),
+    sqlite3 = require('sqlite3').verbose();
+
 
 describe("Movie", function() {
+  beforeEach(function(){
+    movie = new Movie();
+  })
+
+  it("can be instantiated", function(){
+    assert.equal(movie instanceof Movie, true);
+  })
 
   it("has a find_by property that is a function", function() {
-    var movie = new Movie();
     assert.equal(typeof movie.find_by, "function");
   });
+
+  describe("movie queries", function(){
+    beforeEach(function(done){
+      movie = new Movie();
+
+      db_cleaner = new sqlite3.Database('db/test.db');
+
+      db_cleaner.serialize(function(){
+        db_cleaner.exec(
+          "BEGIN; \
+          DELETE FROM movies; \
+          INSERT INTO movies(title, overview, release_date, inventory, available) \
+          VALUES('Jaws', 'Shark!', 'Yesterday', 10, 10), \
+                ('Maws', 'Worm!', 'Yesterday', 11, 11); \
+          COMMIT;",
+          function(err) {
+            db_cleaner.close();
+            done();
+          }
+        );
+      })
+    })
+
+    it("finds 'Jaws' title in the movies table", function(done) {
+      movie.find_by('title', "Jaws", function(err, result){
+        assert.equal(result[0].title, 'Jaws');
+        done();
+      });
+    })
+  })
 });
