1f9d5c02ba7108a6dee5a156256890d94a37abd3
Shanna Chau <shannachau@gmail.com>
2015-09-18 10:28:56 -0700

    Seeded database with customers and movies

--- utils/seed.js
+++ utils/seed.js
@@ -4,29 +4,50 @@ var sqlite3 = require('sqlite3').verbose(),
     db_env = process.env.DB || 'development',
     db = new sqlite3.Database('db/' + db_env + '.db');
 
-// FOR REFERENCE:
-// var movies = require('./movies');
-// //insert into movies(title, inventory) values("Jaws", 10);
-// var movie_statement = db.prepare(
-//   "INSERT INTO movies(title, overview, inventory, release_date) \
-//   VALUES (?, ?, ?, ?);"
-// );
-//
-// db.serialize(function() {
-//   // loop them movies
-//   for(var i = 0; i < movies.length; i++) {
-//     var movie = movies[i];
-//
-//     // insert each one into the db
-//     movie_statement.run(
-//       movie.title,
-//       movie.overview,
-//       movie.inventory,
-//       movie.release_date
-//     );
-//   }
-//
-//   movie_statement.finalize();
-// });
-//
-// db.close(); // ^_^
+var movies = require('./movies');
+var movie_statement = db.prepare(
+  "INSERT INTO movies(title, overview, inventory, release_date) \
+  VALUES (?, ?, ?, ?);"
+);
+
+db.serialize(function() {
+  for(var i = 0; i < movies.length; i++) {
+    var movie = movies[i];
+
+    movie_statement.run(
+      movie.title,
+      movie.overview,
+      movie.inventory,
+      movie.release_date
+    );
+  }
+
+  movie_statement.finalize();
+});
+
+var customers = require('./customers');
+var customer_statement = db.prepare(
+  "INSERT INTO customers(name, registered_at, address, city, state, postal_zip, phone_number, credit) \
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
+);
+
+db.serialize(function() {
+  for(var i = 0; i < customers.length; i++) {
+    var customer = customers[i];
+
+    customer_statement.run(
+      customer.name,
+      customer.registered_at,
+      customer.address,
+      customer.city,
+      customer.state,
+      customer.postal_zip,
+      customer.phone_number,
+      customer.credit
+    );
+  }
+
+  customer_statement.finalize();
+});
+
+db.close();
