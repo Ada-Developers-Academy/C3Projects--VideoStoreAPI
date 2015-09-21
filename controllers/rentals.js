8dd14cdbed74670f90f72c698c6ae23da0dcc6e0
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-21 14:37:08 -0700

    Removed some comments.

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -40,34 +40,14 @@ exports.rentalsController = {
   // POST /rentals/check_out(cust id, movie title) (math for checkout cost)
   // creating a new rental with no returned date
   create: function(req, res) {
-    console.log(req["req"]["body"]);
-
-    // return res.status(200).json(req["req"]["body"]);
-    // return res.redirect('index', { title: 'Express' });
-
     var data = req["req"]["body"]
-
     var db = new Customer();
+
     db.create(data, function(err, result) {
-
       console.log("DONE");
+      // return res.status(200).json(req["req"]["body"]);
+      // return res.redirect('index', { title: 'Express' });
     });
-
-    // if (req.method == 'POST') {
-    //   var body = req.body;
-      // req.on('data', function (data) {
-      //   body += data;
-      //
-      //   // Too much POST data, kill the connection!
-      //   if (body.length > 1e6)
-      //       request.connection.destroy();
-      // });
-      // req.on('end', function () {
-      //   var post = qs.parse(body);
-      //
-      //   // use post['blah'], etc.
-      // });
-    // }
   }
 
 /*
