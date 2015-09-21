2038017e30e0404281c0552e5185dde18bf75723
Ashley Watkins <AWatkins31@gmail.com>
2015-09-21 13:06:06 -0700

    Bug Fix: if there's an err during create, our res code will break.

--- models/rental.js
+++ models/rental.js
@@ -9,15 +9,19 @@ function Rental() {
 Rental.prototype = require('./database').prototype;
 
 Rental.prototype.checkIn = function checkIn(movie_title, date, callback) {
-    var db = this.openDB();
-    var statement = "UPDATE rentals SET return_date = ? WHERE movie_title LIKE ?;";
-    var values = [date, movie_title];
+  var db = this.openDB();
+  var statement = "UPDATE rentals SET return_date = ? WHERE movie_title LIKE ?;";
+  var values = [date, movie_title];
 
-    db.run(statement, values, function(err) {
-      // TODO / FIXME: if there's an error, `this` doesn't exist #changes
-      callback(err, { changed: this.changes });
-      db.close();
-    });
-  }
+  db.run(statement, values, function(err) {
+    if (err) {
+      callback(err, { insertedID: null, changed: null });
+    } else {
+      callback(err, { insertedID: this.lastID, changed: this.changes });
+    }
+
+    db.close();
+  });
+}
 
 module.exports = Rental;
