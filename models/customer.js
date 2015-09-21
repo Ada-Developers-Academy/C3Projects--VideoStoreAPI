d7423b78ace62010bd0eeabd92b51d9f488772f9
Victoria <vikshab2010@hotmail.com>
2015-09-21 13:45:13 -0700

    Test for find_all customers is green

--- models/customer.js
+++ models/customer.js
@@ -6,7 +6,5 @@ function Customer() {
 
 Customer.prototype = require('../database');
 var customer = new Customer();
-// customer.find_by("title", "Jaws", function(err, res){});
 customer.find_all(function(err, res){});
-// customer.sort_by("title", function(err, res){});
 module.exports = Customer;
