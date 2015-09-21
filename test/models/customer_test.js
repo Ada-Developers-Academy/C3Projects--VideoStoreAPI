93d8eb828a6ffb2198a683a87ed3cd21d2a5df9a
Ashley Watkins <AWatkins31@gmail.com>
2015-09-21 11:04:44 -0700

    Adds test for Customer#create

--- test/models/customer_test.js
+++ test/models/customer_test.js
@@ -20,4 +20,26 @@ describe('Customer', function() {
   it('holds onto the `path` to the database', function() {
     assert.equal(customer.dbPath(), dbPath);
   });
+
+  describe('#create', function() {
+    it('creates a new customer record', function(done) {
+     var data = {
+      name: 'Customer1',
+      registered_at: 'Yesterday',
+      address: '1234 Nowhere St',
+      city: 'Nowhereville',
+      state: 'NW',
+      postal_code: '12345',
+      phone: '123-456-7890',
+      account_balance: '20.45'
+    }
+
+     customer.create(data, function(err, res) {
+       assert.equal(err, undefined);
+       assert.equal(res.insertedID, numSeeded + 1);
+       assert.equal(res.changed, 1);
+       done();
+     });
+    });
+  });
 });
