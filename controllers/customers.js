416f7c7f995085e3efede962221b2c8f53caf171
Corinne Pingul <corinnepingul@gmail.com>
2015-09-21 09:37:25 -0700

    Refactors: pulls out customers variable to give it a larger scope.

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,18 +1,17 @@
 "use strict";
 
 var Customer = require('../customers');
+var customer = new Customer();
 
 exports.customersController = {
 
   all: function all(req, res) {
-    var customer = new Customer();
     var customers = customer.all(function(customers) {
       return res.status(200).json(customers);
     });
   },
 
   registered_at_sort: function registered_at_sort(req, res) {
-    var customer = new Customer();
     var sort_type = "registered_at";
     var records_per_page = req.params.records_per_page;
     var offset = req.params.offset;
@@ -22,7 +21,6 @@ exports.customersController = {
   },
 
 name_sort: function name_sort(req, res) {
-  var customer = new Customer();
   var sort_type = "name";
   var records_per_page = req.params.records_per_page;
   var offset = req.params.offset;
