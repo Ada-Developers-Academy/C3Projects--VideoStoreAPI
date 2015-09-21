009aa0d47fd49db5d66eb3bc2d182d550b349467
Kari <kari.lonczak@gmail.com>
2015-09-21 11:00:50 -0700

    Callback Hell insanity and then we discovered join queries...

--- controllers/customers.js
+++ controllers/customers.js
@@ -24,38 +24,67 @@ var customersController = {
   },
 
   customer: function(req, callback) {
-    var statement = "SELECT * FROM customers WHERE id = " + req.params.id + ";";
-    var customer_renting_statment = 
-      "SELECT * FROM rentals \
-      WHERE customer_id = " + req.params.id + "AND \
-      return_date IS NULL;";
+    var json_result = {};
 
-    var customer_rented_statment =
+    var statement = "SELECT * FROM customers WHERE id = " + req.params.id + ";";
+    var customer_renting_statement = 
+      "SELECT * FROM rentals WHERE customer_id = " + req.params.id + " AND return_date IS NULL;";
+
+    var customer_rented_statement =
       "SELECT * FROM rentals \
-      WHERE customer_id = " + req.params.id + "AND \
+      WHERE customer_id = " + req.params.id + " AND \
       return_date IS NOT NULL;";
 
     var db = new Database('db/development.db');
 
-    var customer_info = db.query(statement, function(err, result) {
-      return result[0];
-    });
+    function firstQuery(err, this_query, callback) { 
+      db.query(this_query, function(err, result) {
+        json_result.customer = result[0];
+      });
 
-    var customer_renting = db.query(customer_renting_statment, function(err, result) {
-      return result;
-    })
-
-    var customer_rented = db.query(customer_rented_statment, function(err, result) {
-      return result;
-    })
-
-    console.log(customer_info);
-
-    var json_result = {
-      customer: customer_info,
-      renting: customer_renting,
-      rented: customer_rented 
+      callback(err, customer_renting_statement, thirdQuery);
     };
+
+      function secondQuery(err, this_query, callback) {
+        db.query(this_query, function(err, result) {
+          json_result.renting = result;
+        });
+
+        callback(err, customer_rented_statement);
+      };
+
+        function thirdQuery(err, this_query) {
+          db.query(customer_rented_statement, function(err, result) {
+            json_result.rented = result;
+            console.log(json_result);
+          });
+        };
+
+
+    firstQuery(err, statement, secondQuery);
+    
+
+    // var customer_info = db.query(statement, function(err, result) {
+    //   json_result.customer = result[0];
+    // });
+
+    // var customer_renting = db.query(customer_renting_statement, function(err, result) {
+    //   json_result.renting = result;
+    // })
+
+    // var customer_rented = db.query(customer_rented_statement, function(err, result) {
+    //   json_result.rented = result;
+    // })
+
+    // console.log("customer_info = " + customer_info);
+    // console.log("customer_renting = " + customer_renting);
+    // console.log("customer_rented = " + customer_rented);
+
+    // var json_result = {
+    //   customer: customer_info,
+    //   renting: customer_renting,
+    //   rented: customer_rented 
+    // };
 
     callback(err, json_result);
   }
