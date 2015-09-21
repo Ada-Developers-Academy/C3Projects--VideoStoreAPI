fd73407cea2f880ecd848ad6c8ecc7f5122ec188
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-21 09:15:01 -0700

    Removed a lot of my commented-out code.

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,13 +1,6 @@
 "use strict";
 
 var Customer = require('../models/customers');
-// var Rentals = require('../models/rentals');
-
-// // ALTERNATIVE WAY OF CODING THIS:
-// function CustomersController() {
-//   // CODE HERE
-// }
-// module.exports = CustomersController;
 
 exports.customersController = {
   // GET /customers
@@ -18,36 +11,26 @@ exports.customersController = {
       return res.status(200).json(result);
     });
   },
+
   // GET /customers/:id
   show: function(req, res) {
-    // (currently checkout_out movies, rental history)
+    // returns currently checkout_out movies & rental history
     var id = req["params"]["id"];
     var db = new Customer();
     var data = db.find_checked_out(id, function(err, result) {
-      // console.log(result);
-      // var db_rentals = new Rentals();
-      // var dataing = db_rentals.find_by("")
-      // need to receive checked_out movies by customer id
-      // need to recieve rental history by customer id
-
       return res.status(200).json(result);
     });
-  // for each customer, add that customer to results
-  // for (var i = 0; i < data.length; i++) {
-  //   results.customers[i] = {
-  //     "id": "yo",
-  //     "name": "yo",
-  //   }
-  // }
-  }
+
   // GET /customers/by_name?n=XXX&p=XXX
   // showByName: function(req, res) {
   //   var Customer = new Customer();
   // },
+
   // // GET /customers/by_registered_at?n=XXX&p=XXX
   // showByRegistered_at: function(req, res) {
   //
   // },
+
   // // GET /customers/by_postal_code?n=XXX&p=XXX
   // showByPostalCode: function(req, res) {
   //
