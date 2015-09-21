caeb81fa9fa1505288a83b7d6b6a50ec5607c00d
Corinne Pingul <corinnepingul@gmail.com>
2015-09-21 10:01:34 -0700

    Refactor: Changes name of function to SortCustomers.

--- controllers/customers.js
+++ controllers/customers.js
@@ -3,7 +3,7 @@
 var Customer = require('../customers');
 var customer = new Customer();
 
-var sortInfo = function(sort_type, params, res) {
+var sortCustomers = function(sort_type, params, res) {
   var records_per_page = params.records_per_page;
   var offset = params.offset;
   var customers = customer.sort_by(sort_type, records_per_page, offset, function(customers) {
@@ -21,16 +21,16 @@ exports.customersController = {
 
   registered_at_sort: function registered_at_sort(req, res) {
     var sort_type = "registered_at";
-    sortInfo(sort_type, req.params, res);
+    sortCustomers(sort_type, req.params, res);
   },
 
   name_sort: function name_sort(req, res) {
     var sort_type = "name";
-    sortInfo(sort_type, req.params, res);
+    sortCustomers(sort_type, req.params, res);
   },
 
   postal_code_sort: function postal_code_sort(req, res) {
     var sort_type = "postal_code";
-    sortInfo(sort_type, req.params, res);
+    sortCustomers(sort_type, req.params, res);
   }
 }
