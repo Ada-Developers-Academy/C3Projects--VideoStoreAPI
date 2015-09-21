00538a34c781bd77067d00803386d4c40f9d8967
Kyna <khn710@gmail.com>
2015-09-21 09:13:10 -0700

    WIP: model testing for customers

--- models/movies.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
- function Movie() {
-   this.table_name = "movies";
- }
-
- Movie.prototype = require("../database");
-
- module.exports = Movie
--- test/customers_tests.js
+++ test/customers_tests.js
@@ -1,21 +1,21 @@
 var assert = require('assert'),
-    Customer  = require('../controllers/customers'),
+    Customer = require('../models/customers'),
     sqlite3 = require('sqlite3').verbose();
 
 describe("Customer", function() {
-  var movie, db_cleaner
+  var customer, db_cleaner;
 
   beforeEach(function(done) {
-    movie = new Customer();
+    customer = new Customer();
 
     db_cleaner = new sqlite3.Database('db/test.db');
     db_cleaner.serialize(function() {
       db_cleaner.exec(
         "BEGIN; \
-        DELETE FROM movies; \
-        INSERT INTO movies(title, overview, release_date, inventory) \
-        VALUES('Jaws', 'Shark!', 'Yesterday', 10), \
-              ('Maws', 'Worm!', 'Yesterday', 11); \
+        DELETE FROM customers; \
+        INSERT INTO customers(name, registered_at, address, postal_code, account_credit) \
+        VALUES('Jim', 'Wed, 29 Apr 2015 07:54:14 -0700', '1800 Some Place', '90210', 17.38), \
+              ('Jack', 'Tue, 4 Apr 2015 07:54:14 -0700', '555 Some Where', '90210', 19.84); \
         COMMIT;"
         , function(err) {
           db_cleaner.close();
@@ -26,12 +26,12 @@ describe("Customer", function() {
   })
 
   it("can be instantiated", function() {
-    assert(movie instanceof Customer);
+    assert(customer instanceof Customer);
   })
 
   describe("instance methods", function() {
-    it("can find a movie by id", function(done){
-      movie.find_by("id", 1, function(err, res) {
+    it("can find a customer by id", function(done){
+      customer.find_by("id", 1, function(err, res) {
         assert.equal(err, undefined);
         assert(res instanceof Array);
         assert.equal(res.length, 1);
@@ -40,21 +40,21 @@ describe("Customer", function() {
       })
     })
 
-    it("can find a movie by title", function(done) {
-      movie.find_by("title", "Jaws", function(err, res) {
+    it("can find a customer by name", function(done) {
+      customer.find_by("name", "Jim", function(err, res) {
         assert.equal(err, undefined);
         assert(res instanceof Array);
         assert.equal(res.length, 1);
-        assert.equal(res[0].title, 'Jaws');
+        assert.equal(res[0].name, 'Jim');
         done();
       })
     })
 
-    it("can save changes to a movie", function(done) {
-      movie.find_by("title", "Jaws", function(err, res) {
+    it("can save changes to a customer", function(done) {
+      customer.find_by("title", "Jaws", function(err, res) {
         var original_title = res[0].title;
         var id = res[0].id;
-        movie.save({title: "Jaws 2: Jawsier", id: id}, function(err, res) {
+        customer.save({title: "Jaws 2: Jawsier", id: id}, function(err, res) {
           assert.equal(err, undefined);
           assert.equal(res.inserted_id, 0); //it didn't insert any records
           assert.equal(res.changed, 1); //it updated one record
@@ -63,7 +63,7 @@ describe("Customer", function() {
       })
     });
 
-    it("can save a new movie to the database", function(done) {
+    it("can save a new customer to the database", function(done) {
       var data = {
         title: "RoboJaws",
         overview: "Jaws is hunted by RoboJaws",
@@ -71,13 +71,13 @@ describe("Customer", function() {
         inventory: 10
       }
 
-      movie.create(data, function(err, res) {
+      customer.create(data, function(err, res) {
         assert.equal(res.inserted_id, 3); //it inserted a new record
         assert.equal(res.changed, 1); //one record was changed
 
-        movie.find_by("title", "RoboJaws", function(err, res) {
+        customer.find_by("title", "RoboJaws", function(err, res) {
           assert.equal(res.length, 1);
-          assert.equal(res[0].title, 'RoboJaws'); //we found our new movie
+          assert.equal(res[0].title, 'RoboJaws'); //we found our new customer
           done();
         })
       })
