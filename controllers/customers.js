1ca9b997e810f90055bef3327845aa26c7972075
Corinne Pingul <corinnepingul@gmail.com>
2015-09-21 09:59:57 -0700

    Refactor: pulls out sort info into separate function. This function now calls the model function.

--- controllers/customers.js
+++ controllers/customers.js
@@ -3,6 +3,14 @@
 var Customer = require('../customers');
 var customer = new Customer();
 
+var sortInfo = function(sort_type, params, res) {
+  var records_per_page = params.records_per_page;
+  var offset = params.offset;
+  var customers = customer.sort_by(sort_type, records_per_page, offset, function(customers) {
+    return res.status(200).json(customers);
+  });
+}
+
 exports.customersController = {
 
   all: function all(req, res) {
@@ -13,28 +21,16 @@ exports.customersController = {
 
   registered_at_sort: function registered_at_sort(req, res) {
     var sort_type = "registered_at";
-    var records_per_page = req.params.records_per_page;
-    var offset = req.params.offset;
-    var customers = customer.sort_by(sort_type, records_per_page, offset, function(customers) {
-      return res.status(200).json(customers);
-    });
+    sortInfo(sort_type, req.params, res);
   },
 
   name_sort: function name_sort(req, res) {
     var sort_type = "name";
-    var records_per_page = req.params.records_per_page;
-    var offset = req.params.offset;
-    var customers = customer.sort_by(sort_type, records_per_page, offset, function(customers) {
-      return res.status(200).json(customers);
-    });
+    sortInfo(sort_type, req.params, res);
   },
 
   postal_code_sort: function postal_code_sort(req, res) {
     var sort_type = "postal_code";
-    var records_per_page = req.params.records_per_page;
-    var offset = req.params.offset;
-    var customers = customer.sort_by(sort_type, records_per_page, offset, function(customers) {
-      return res.status(200).json(customers);
-    });
+    sortInfo(sort_type, req.params, res);
   }
 }
