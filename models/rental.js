8cf32b0a9937787dc41e086abb646ee2e4b1a06e
Sally Moore <sallyamoore@gmail.com>
2015-09-21 10:15:15 -0700

    WIP post to rentals. Not working yet. Moving on to something else till we can get some help.

--- models/rental.js
+++ models/rental.js
@@ -13,18 +13,34 @@ Rental.prototype = {
     var db = new sqlite3.Database('db/' + db_env + '.db');
     var rental_duration_days = 7;
     var return_date = new Date(Date.now() + (rental_duration_days * 24 * 60 * 60 * 1000));
-    // var movie_id = "SELECT id FROM movies WHERE title = '" + title + "';";
+    var movie_id = 1;
 
     // have to actually *run* the sqlite3 query and get result... above doesn't do that...
     // not sure if this works either. Seems to return a db connection object...WIP
-    var movie_id = db.get("SELECT id FROM movies WHERE title = '" + title + "';");
 
-    var statement = "INSERT INTO " + this.table_name + " (return_date, movie_id, customer_id, checked_out) " + "VALUES (" + return_date  + ", " + movie_id + ", " + id + ", 'true'); ";
 
-    db.run(statement, function(err) {
-      callback(err, { inserted_id: this.lastID, changed: this.changes });
+    // var movie_id = function() {
+    //   db.get("SELECT id FROM movies WHERE title = '" + title + "';");
+    //   return this.lastID; //in callback?
+    // };
+    // console.log(movie_id());
+
+
+    // db.serialize(function() {
+      // db.run("SELECT id FROM movies WHERE title = '" + title + "';"), function(err) {
+      //   // console.log(this.lastID);
+      //   callback(err, { movie_id: this.lastID });
+      // };
+      //
+      var statement = "INSERT INTO " + this.table_name + " (return_date, movie_id, customer_id, checked_out) " + "VALUES (" + return_date  + ", " + movie_id + ", " + id + ", 'true'); ";
+
+      db.run(statement, function(err) {
+        callback(err, { inserted_id: this.lastID, changed: this.changes });
+      });
       db.close();
-    });
+    // })
+
+
     //
     // db.all(statement, function(err, res) {
     //   if (callback) callback(err, res);
--- routes/rentals.js
+++ routes/rentals.js
@@ -5,10 +5,11 @@ var rentals_exports = require('../controllers/rentals');
 /* POST /rentals/checkout/:id/:title */
 /* keeps looking for a GET... WIP */
 // router.get('/checkout/:id/:title', function(req, res, next) {
-//   return rentals_exports.rentalsController.create(req, res);
+  // return rentals_exports.rentalsController.create(req, res);
 // });
 
 router.post('/checkout/:id/:title', function(req, res, next) {
+  console.log("id: " + req.param(id));
   return rentals_exports.rentalsController.create(req, res);
 });
 
