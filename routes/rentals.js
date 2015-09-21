37b5b4835549e0966824c42e7ca794e699759d8f
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-09-21 14:21:06 -0700

    WIP; fixing logic on checkout route

--- routes/rentals.js
+++ routes/rentals.js
@@ -10,36 +10,47 @@ var Movie = require('../models/movie'),
 var RENTAL_PERIOD = 5; // 5 days
 
 router.post('/checkout/:customer_id/:movie_id', function(req, res, next) {
-  // add check for if there is an available movie for checkout:
+  // Add check for if there is an available movie for checkout:
   // query db to count # of rentals with that movie id
-  // check movie record to get inventory
-  // compare # of rentals with inventory
-  // if inventory <= rentals, return message NO
-  // else proceed with checkout
+  // TODO: check only returned = false
   var movie_id = req.params.movie_id;
-  var rented_movies_count = movie.where(['id'], movie_id, function(err, rows) {
-    return rows.length;
+  var rented_movies_count = movie.where(['id', 'returned'], [movie_id, 'false'], function(err, rows) {
+    rows.length;
   });
 
-  
-  
-
-  var values = [];
-  values.push(req.params.customer_id);
-  values.push(movie_id);
-
-  var checkout_date = new Date();
-  var return_date = new Date();
-  return_date.setDate(return_date.getDate() + RENTAL_PERIOD);
-
-  var defaults = [checkout_date, return_date, "false"];
-
-  values = values + defaults;
-  var columns = ['customer_id', 'movie_id', 'checkout_date', 'return_date', 'returned'];
-
-  rental.create(columns, values, function(err, res) {
-    res.status(200).json(res);
+  // check movie record to get inventory
+  var relevant_movie = movie.find_by('id', movie_id, function(err, row) {
+    row;
   });
+
+  var inventory = relevant_movie[0].inventory;
+  console.log(inventory);
+  // compare # of rentals with inventory
+  if (rented_movies_count >= inventory) {
+    // if inventory <= rentals, return message NO
+    res.status(403).json({ error: "There are no available copies of that movie for rental." })
+  } else {
+    // else proceed with checkout
+    var values = [];
+    values.push(req.params.customer_id);
+    values.push(movie_id);
+
+    var checkout_date = new Date();
+    var return_date = new Date();
+    return_date.setDate(return_date.getDate() + RENTAL_PERIOD);
+
+    var defaults = [checkout_date, return_date, "false"];
+
+    for (var i = 0; i < defaults.length; i++) {
+      values.push(defaults[i]);
+    }
+
+    var columns = ['customer_id', 'movie_id', 'checkout_date', 'return_date', 'returned'];
+
+    rental.create(columns, values, function(err, res) {
+      res.status(200).json(res);
+    });
+  }
 });
 
 module.exports = router;
