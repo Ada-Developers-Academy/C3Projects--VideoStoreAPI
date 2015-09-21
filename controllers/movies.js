a856e59c008fcfac9002c1cf300e08bf474c1e34
Brenna <brennaleker@gmail.com>
2015-09-21 09:59:55 -0700

    refactored movies controller, added sortInfo variable to handle repeated code

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,6 +3,14 @@
 var Movie = require('../movies');
 var movie = new Movie();
 
+var sortInfo = function (sort_type, params, res) {
+  var records_per_page = params.records_per_page;
+  var offset = params.offset;
+  var movies = movie.sort_by(sort_type, records_per_page, offset, function(movies){
+    return res.status(200).json(movies);
+  });
+}
+
 exports.moviesController = {
 
   all: function all(req, res) {
@@ -13,19 +21,12 @@ exports.moviesController = {
 
   release_date_sort: function release_date_sort(req, res) {
     var sort_type = "release_date";
-    var records_per_page = req.params.records_per_page;
-    var offset = req.params.offset;
-    var movies = movie.sort_by(sort_type, records_per_page, offset, function(movies){
-      return res.status(200).json(movies);
-    });
+    sortInfo(sort_type, req.params, res);
   },
 
   title_sort: function title_sort(req, res) {
     var sort_type = "title";
-    var records_per_page = req.params.records_per_page;
-    var offset = req.params.offset;
-    var movies = movie.sort_by(sort_type, records_per_page, offset, function(movies){
-      return res.status(200).json(movies);
-    });
+    sortInfo(sort_type, req.params, res);
   }
+
 }
