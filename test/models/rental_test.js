4a3c05f23ec4e514d278c9f31c6cae6aa2b944c9
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-21 15:01:39 -0700

    added tests for #findBy

--- test/models/rental_test.js
+++ test/models/rental_test.js
@@ -54,6 +54,44 @@ describe('Rental', function() {
     });
   });
 
+  describe('#findBy', function() {
+    it("returns 1 rental where the movie_title is 'Wait Until Dark'", function(done) {
+      rental.findBy('movie_title', 'Wait Until Dark', function(err, rows) {
+        assert.equal(err, undefined);
+        assert.equal(rows.length, 1);
+        assert.equal(rows[0].movie_title, 'Wait Until Dark');
+        done();
+      });
+    });
+
+    it("'WAIT UNTIL dark' returns 1 rental where the movie_title is 'Wait Until Dark'", function(done) {
+      rental.findBy('movie_title', 'WAIT UNTIL dark', function(err, rows) {
+        assert.equal(err, undefined);
+        assert.equal(rows.length, 1);
+        assert.equal(rows[0].movie_title, 'Wait Until Dark');
+        done();
+      });
+    });
+
+    it('returns 1 rental where the return_date is an empty string', function(done) {
+      rental.findBy('return_date', "", function(err, rows) {
+        assert.equal(err, undefined);
+        assert.equal(rows.length, 1);
+        assert.equal(rows[0].movie_title, 'Wait Until Dark');
+        done();
+      });
+    });
+
+    it('returns an error when an unrecognized column is provided', function(done) {
+      rental.findBy('badColumnName', 'North by Northwest', function(err, rows) {
+        assert(err);
+        assert.equal(err.message, 'Error: syntax error. Unrecognized parameter.');
+        assert.equal(rows, undefined);
+        done();
+      });
+    });
+  });
+
   describe('#checkIn', function() {
     it('checks in a rental by adding a return date', function(done) {
       var movie_title = 'Wait Until Dark';
