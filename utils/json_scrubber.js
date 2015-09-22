0e09325491476295bf510959b2e2a259815599f3
Elsa <elsa.moluf@gmail.com>
2015-09-21 19:48:14 -0700

    Cleaned up to prep more WIP.

--- utils/json_scrubber.js
+++ utils/json_scrubber.js
@@ -10,24 +10,14 @@ var json_array = [];
     var formatted_date = date.toISOString();
 
     customer.registered_at = formatted_date.split("T")[0];
-    // console.log("customer.registered_at " + customer.registered_at);
-
-    // customer.registered_at
     json_array.push(JSON.stringify(customer));
   }
-  console.log("json_array " + json_array[0]);
+  
+console.log("json_array " + json_array[0]);
 
-    // var parsed_json_array = JSON.parse(json_array);
-    // console.log("parsed json_array" + JSON.parse(json_array[0]));
-
-    fs.writeFile('formatted_customers.json', json_array, function(err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-    });
-
-
-
-  // console.log("inside writeFile ", customers[0]);
-  // var json_customers =
+fs.writeFile('formatted_customers.json', json_array, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
