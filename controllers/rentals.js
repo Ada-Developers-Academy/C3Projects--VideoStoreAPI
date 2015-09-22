29a2163a3d3f34a75dbc9925f6a282ab18c7b608
Kari <kari.lonczak@gmail.com>
2015-09-22 12:10:17 -0700

    Can now post a new rental record to our db. WIP on having it also update the movie's inventory count and the customer's account credit.

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -71,6 +71,49 @@ var rentalsController = {
 
       callback(err, json_result);
     });
+  },
+
+  checkout_movie: function(req, callback) {
+    // creating a new rental record
+    // reducing inventory available for that movie -1
+    // reducing customer's account credit by $3
+
+    // var statement = 
+    //   "INSERT INTO rentals (customer_id, name, movie_id, title, checkout_date, due_date, return_date) \
+    //   VALUES (" + req.params.customer_id + ", " + CUSTOMER_NAME + ", " + req.params.movie_id 
+    //   + ", " + MOVIE_TITLE + ", date('now'), date('now', '+7 days', NULL); \
+
+    //   UPDATE movies SET inventory_avail = inventory_avail - 1 WHERE ID = " + req.params.movie_id + "; \
+    //   UPDATE customers SET account_credit = account_credit - 3 WHERE ID = " + req.params.customer_id + ";"; 
+
+    var customer_id = parseInt(req.params.customer_id);
+    var movie_id = parseInt(req.params.movie_id);
+
+    var statement = 
+      "INSERT INTO rentals (customer_id, name, movie_id, title, checkout_date, due_date, return_date) \
+      VALUES (" + customer_id + ", 'test customer name', " + movie_id + ", 'test movie name', date('now'), date('now', '+7 days'), NULL);";
+      // UPDATE movies SET inventory_avail = inventory_avail - 1 WHERE ID = " + req.params.movie_id + "; \
+      // UPDATE customers SET account_credit = account_credit - 3 WHERE ID = " + req.params.customer_id + "; \
+      // SELECT rentals.id, rentals.title, rentals.customer_id, rentals.name, \
+      //   rentals.checkout_date, rentals.due_date, rentals.return_date \
+      // FROM movies, rentals \
+      // ORDER BY id DESC \
+      // LIMIT 1";
+    
+
+    var db = new Database('db/development.db');         
+      db.query(statement, function(err, result) {
+
+        console.log(statement);
+        console.log(err);
+
+        var json_result = {
+          rental: result
+        };
+
+        callback(err, json_result);
+    });
+
   }
 };
 
