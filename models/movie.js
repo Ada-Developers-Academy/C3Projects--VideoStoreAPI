6ea924c491f852375e55f4ba647701407f9c3f84
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-22 15:39:36 -0700

    added #customersCurrent to get a list of customers who currently have a given movie checked out

--- models/movie.js
+++ models/movie.js
@@ -16,4 +16,15 @@ function Movie() {
 // this is silly-ish, but necessary because of how we set up the DB object
 Movie.prototype = require('./database').prototype;
 
+Movie.prototype.customersCurrent = function customersCurrent(movieTitle, callback) {
+  var db = this.openDB();
+  var statement = 'SELECT customers.id AS customers_id, customers.name, rentals.id FROM customers INNER JOIN rentals ON customers.id = rentals.customer_id WHERE rentals.movie_title LIKE ? AND rentals.return_date = "";';
+  var values = movieTitle;
+
+  db.all(statement, values, function(err, rows) {
+    callback(err, rows);
+    db.close();
+  });
+}
+
 module.exports = Movie;
