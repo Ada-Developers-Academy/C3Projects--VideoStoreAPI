d79d06db18f1fa6a25a4743f25e091ae0d245100
marleigh <camarleigh@gmail.com>
2015-09-22 11:12:31 -0700

    brnch guides for rentals

--- routes/rentals.js
+++ routes/rentals.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// *GET*  rental/title
+
+// *GET*  rental/title/available
+
+// *GET*  rental/title/customers
+
+// *POST* rental/title/:id/checkout
+
+// *POST* rental/title/:id/checkin
+
+// *GET*  rental/customers/overdue
 module.exports = router;
