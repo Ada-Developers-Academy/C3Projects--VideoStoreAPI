6caad0444196ff4e7b39881cea78a5d01efcf0ad
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-22 16:19:11 -0700

    Takes care of movies\/:title\/renting endpoint tests.

--- test/controllers/movieControllerTest.js
+++ test/controllers/movieControllerTest.js
@@ -8,7 +8,7 @@ var agent = request.agent(app);
 var movieController = require('../../controllers/movieController');
 
 describe('movie controller', function() {
-  describe.only('GET /movies/all/:page', function() {
+  describe('GET /movies/all/:page', function() {
     it('responds with json format', function(done) {
       agent.get('/movies/all/1').set('Accept', 'application/json')
         .expect('Content-Type', /json/)
@@ -32,7 +32,6 @@ describe('movie controller', function() {
       function(done) {
 
       var movie_keys = [
-        'id',
         'title',
         'overview',
         'release_date',
@@ -48,5 +47,79 @@ describe('movie controller', function() {
     })
   }) // GET /movies
 
+  describe("GET /movies/:title", function() {
+    it('responds with json format', function(done) {
+      agent.get('/movies/Alien').set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, function(error, result) {
+        assert.equal(error, undefined);
+      })
+      done();
+    })
 
+    it('responds with 1 movie result', function(done) {
+      agent.get('/movies/Alien').set('Accept', 'application/json')
+        .expect('Content-Length', '1')
+        .expect(200, function(error, result) {
+          assert.equal(error, undefined);
+        })
+      done();
+    })
+
+    it("responds with keys of 'id', \
+      'title', 'overview', 'release_date' and 'inventory'",
+      function(done) {
+
+      var movie_keys = [
+        'title',
+        'overview',
+        'release_date',
+        'inventory'
+      ];
+
+      agent.get('/movies/Alien').set('Accept', 'application/json')
+        .expect(200, function(error, result) {
+          assert.deepEqual(Object.keys(result.body[0]), movie_keys);
+        })
+      done();
+    })
+  }) // GET /movies/:title
+
+  describe('GET /movies/:title/renting', function() {
+    it('responds with json format', function(done) {
+      agent.get('/movies/Alien/renting').set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, function(error, result) {
+        assert.equal(error, undefined);
+      })
+      done();
+    })
+
+    it('responds with an array of customers', function(done) {
+      agent.get('/movies/Alien/renting').set('Accept', 'application/json')
+        .expect('Content-Length', '200')
+        .expect(200, function(error, result) {
+          assert.equal(error, undefined);
+        })
+      done();
+    })
+
+    it("responds with keys of 'id', \
+      'title', 'overview', 'release_date' and 'inventory'",
+      function(done) {
+
+      var movie_keys = [
+        'title',
+        'overview',
+        'release_date',
+        'inventory'
+      ];
+
+      agent.get('/movies/Alien/renting').set('Accept', 'application/json')
+        .expect(200, function(error, result) {
+          assert.deepEqual(Object.keys(result.body[0]), movie_keys);
+        })
+      done();
+    })
+  })
 }) // moviecontroller describe
