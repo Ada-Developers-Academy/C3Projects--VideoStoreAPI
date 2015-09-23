b8c9cabbd6cefc9dda7ebd683be7643707fd7faa
Ashley Watkins <AWatkins31@gmail.com>
2015-09-23 13:59:28 -0700

    WIP -- Adds CustomersController#index setup + limited testing.
    Only includes 1 test to confirm that we're getting JSON back.

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
+app.use('/customers', require('./routes/customers'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
