21d16b342530bc2f5ac37dbeffdccef30139c306
Shanna Chau <shannachau@gmail.com>
2015-09-23 13:59:47 -0700

    Empty scaffolding for test

---
+++ test/controllers/rentals.js
@@ -0,0 +1,8 @@
+var request = require('supertest'),
+assert = require('assert'),
+app = require('../../app'),
+sqlite3 = require('sqlite3').verbose(),
+agent = request.agent(app);
+
+describe("Endpoints under /rentals", function() {
+})
