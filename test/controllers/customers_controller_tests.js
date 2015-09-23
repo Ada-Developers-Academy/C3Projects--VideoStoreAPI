43fa49e8434c0a701900afa07d2c8791f78a635a
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-23 12:02:34 -0700

    Wrote an incorrect test for customers/:id\! Way to get involved and look at the requirements, whoo\!

--- test/controllers/customers_controller_tests.js
+++ test/controllers/customers_controller_tests.js
@@ -31,32 +31,83 @@ describe("Customers Controller", function() {
   })
 
   describe("GET /customers", function() {
-    var movie_request;
+    var customer_request;
 
     beforeEach(function(done) {
-      movie_request = agent
-                      .get('/customers')
-                      .set('Accept', 'application/json');
+      customer_request = agent
+                         .get('/customers')
+                         .set('Accept', 'application/json');
       done();
     })
 
     it("responds with json", function(done) {
-      movie_request
-        .expect('Content-Type', /application\/xmljson/)
+      customer_request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
+        // .end(function(err, res) {
+        //   if (err) return done(err);
+        //   done();
+        // });
+    });
+
+    it("returns an array of all customer objects", function(done) {
+      customer_request
+        .expect(200, function(error, result) {
+          assert.equal(result.body.length, 2);
+
+          var keys = ['id', 'name', 'registered_at', 'address', 'city', 'state', 'postal_code', 'phone', 'account_credit'];
+          assert.deepEqual(Object.keys(result.body[0]), keys);
+          done();
+        })
+    });
+  });
+
+  describe("GET /customers/:id", function() {
+    var customer_request;
+
+    beforeEach(function(done) {
+      customer_request = agent
+                         .get('/customers/1')
+                         .set('Accept', 'application/json');
+      done();
+    })
+
+    it("responds with json", function(done) {
+      customer_request
+        .expect('Content-Type', /application\/json/)
         .expect(200)
         .end(function(err, res) {
           if (err) return done(err);
           done();
         });
-    })
+    });
 
-    // it("returns all customers")
+    it("can find customer with id 1", function(done) {
+      customer_request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, function(err, res) {
+          console.log(res);
+          assert.equal(res.body.length, 1);
+
+          var keys = ['id', 'name', 'registered_at', 'address', 'city', 'state', 'postal_code', 'phone', 'account_credit'];
+          var customer = res.body[0]
+          assert.deepEqual(Object.keys(customer), keys);
+
+          assert.equal(customer.name, 'A');
+          assert.equal(customer.registered_at, '2015-01-01');
+          assert.equal(customer.address, '111 St');
+          assert.equal(customer.city, 'Seattle');
+          assert.equal(customer.state, 'WA');
+          assert.equal(customer.postal_code, '55555');
+          assert.equal(customer.phone, '555-5555');
+          assert.equal(customer.account_credit, 19.50);
+
+          done();
+        })
+    });
+
   });
 
-  // describe("GET /customers/:id", function() {
-  //
-  // });
-  //
   // describe("GET /customers/by_name", function() {
   //
   // });
