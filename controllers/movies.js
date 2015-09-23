ba87bcd0c795ec43ff57c8fd52f9912dcc07c85f
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-23 16:38:18 -0700

    added movie controller

---
+++ controllers/movies.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var Movie = require('../models/movie');
+
+var Controller = {
+  index: function(req, res, next) {
+    new Movie().all(Controller.sendJSON.bind(res));
+  },
+
+  sendJSON: function(err, res) {
+    if (err) {
+      this.status(500).json(err);
+    } else {
+      this.status(200).json(res);
+    }
+  }
+}
+
+module.exports = Controller;
