0ba502e1dcf8e531d9e4ba978232ec132cfc68cc
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-09-23 11:53:07 -0700

    Adds comments to movie route file.

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -39,7 +39,6 @@ module.exports = {
         console.log(err); // error handling
         return;
       }
-
       return response.status(200).json(results);
     });
 
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -4,14 +4,18 @@ var express = require('express');
 var router = express.Router();
 var movieController = require("../controllers/movieController");
 
+// GET a single movie profile
 router.get('/:title', function(req, res, next) {
   return movieController.by_title(req, res);
 });
 
+// GET a list of customers that have rented movie title
 router.get('/:title/renting', function(req, res, next) {
   return movieController.whos_renting(req, res);
 });
 
+// GET a list of customers that have rented title in the past,
+// sorted by customer_id || customer_name || check_out_date
 router.get('/:title/rented/sort_by=:query/:page', function(req, res, next) {
   var sort = req.params.query;
 
@@ -50,6 +54,7 @@ router.get('/:title/rented/sort_by=:query', function(req, res, next) {
     }
 });
 
+// GET all movies
 router.get('/all', function(req, res, next) {
   return movieController.all_movies(req, res);
 });
@@ -57,6 +62,7 @@ router.get('/all/:page', function(req, res, next) {
   return movieController.all_movies(req, res);
 });
 
+// GET all movies sorted by release_date or title
 router.get("/all/sort_by=:sort/:page", function(req, res, next) {
   var sort = req.params.sort;
 
