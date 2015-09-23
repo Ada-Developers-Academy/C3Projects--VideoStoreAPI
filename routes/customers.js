1f9552be1a7722e876a15977f4c31a5ab2f3b0c7
Elia <elgrenier@gmail.com>
2015-09-23 15:39:36 -0700

    deleting unused code

--- routes/customers.js
+++ routes/customers.js
@@ -12,36 +12,3 @@ router.get('/current/:id', Controller.current);
 router.get('/history/:id', Controller.history);
 
 module.exports = router;
-
-// ORIGINAL CODE ---------------------------------------------------------------
-
-// var express = require('express');
-// var router = express.Router();
-// var customers_exports = require('../controllers/customers')
-//
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   return customers_exports.customersController.index(req, res);
-// });
-//
-// router.get('/name/:records/:offset', function(req, res, next) {
-//   return customers_exports.customersController.name(req, res);
-// });
-//
-// router.get('/registered/:records/:offset', function(req, res, next) {
-//   return customers_exports.customersController.registered(req, res);
-// });
-//
-// router.get('/postal/:records/:offset', function(req, res, next) {
-//   return customers_exports.customersController.postal(req, res);
-// });
-//
-// router.get('/current/:id', function(req, res, next) {
-//   return customers_exports.customersController.current(req, res);
-// });
-//
-// router.get('/history/:id', function(req, res, next) {
-//   return customers_exports.customersController.history(req, res);
-// });
-//
-// module.exports = router;
