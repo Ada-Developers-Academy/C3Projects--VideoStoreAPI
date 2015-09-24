a10ed3db5dcc97c8035fbfef4977f09d70785ca0
Lila Garcia <lilagrc@gmail.com>
2015-09-23 21:32:03 -0700

    Added more instance method tests

--- test/models/customer.js
+++ test/models/customer.js
@@ -11,7 +11,7 @@ describe('Customer', function() {
 
    db_cleaner = new sqlite3.Database('db/test.db');
    db_cleaner.serialize(function() {
-     db_cleaner.exec("BEGIN; DELETE FROM customers; INSERT INTO customers(name, registered_at, address, city, state, postal_code, phone, account_credit) VALUES('Mulder', 'yesterday', '123', 'DC', 'DC', '834885', '49583', 5), ('Scully', 'last week', '12 blah','DC', 'DC', '2342', '534', 7); COMMIT;", function(err) {
+     db_cleaner.exec("BEGIN; DELETE FROM customers; DELETE FROM rentals; INSERT INTO customers(name, registered_at, address, city, state, postal_code, phone, account_credit) VALUES('Mulder', 'yesterday', '123', 'DC', 'DC', '834885', '49583', 5), ('Scully', 'last week', '12 blah','DC', 'DC', '2342', '534', 7); INSERT INTO rentals(movie_id, customer_id, returned_date, due_date, checked_out) VALUES(1, 1, '', '2015-09-10', '2015-09-01'), (2, 2, '2015-09-30', '2015-10-01', '2015-09-15'); COMMIT;", function(err) {
          db_cleaner.close();
          done();
        }
@@ -44,6 +44,30 @@ describe('Customer', function() {
         assert.equal(res.length, 1);
 
         assert.equal(res[0].name, 'Scully');
+
+        done();
+      });
+    });
+
+    it("can find a customer's current rentals", function(done) {
+      customer.customer_rentals(1, function(err, res) {
+        assert.equal(err, undefined);
+        assert(res instanceof Array);
+        assert.equal(res.length, 1);
+
+        assert.equal(res[0].title, 'X-files: I want to believe');
+
+        done();
+      });
+    });
+
+    it("can find a customer's past rentals", function(done) {
+      customer.customer_history(2, function(err, res) {
+        assert.equal(err, undefined);
+        assert(res instanceof Array);
+        assert.equal(res.length, 1);
+
+        assert.equal(res[0].title, 'The Lone Gunmen');
 
         done();
       });
