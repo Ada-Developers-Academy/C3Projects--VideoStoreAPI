51ada490a04321424e63143855bc515921db4fe1
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-24 08:56:44 -0700

    added test for /movies with optional sort option

--- test/controllers/movies_test.js
+++ test/controllers/movies_test.js
@@ -56,4 +56,49 @@ describe.only('/movies', function() {
       );
     });
   });
+
+  // ?n=12&p=2&sort=_____
+  // 1. ?n=12&p=2
+  // 2. ?n=12
+  // 3. ?sort=_____
+  // 4. ?n=12&p=2&sort=_____
+
+  describe("GET '/?sort='", function() {
+    var numMoviesSeeded;
+    var request;
+
+    before(function(done) {
+      var data = {
+        movies: [
+          { title: 'Jaws and Maws', overview: 'Worm!', release_date: '2015-09-12', inventory: 11 },
+          { title: 'Jaws', overview: 'Shark!', release_date: '1975-06-19', inventory: 10 },
+          { title: 'The French Connection', overview: 'Bonjour!', release_date: '1971-10-07', inventory: 8 }
+        ]
+      }
+      numMoviesSeeded = data.movies.length;
+      resetTables(data, done);
+    });
+
+    beforeEach(function() {
+      request = agent.get('/movies/?sort=title').set('Accept', 'application/json');
+    });
+
+    it('responds with json', function(done) {
+      request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
+    });
+
+    it('returns all movies sorted by title in the body', function(done){
+      request
+        .expect(200, function(err, res) {
+          assert.equal(err, undefined);
+          assert.equal(res.body.length, numMoviesSeeded);
+          assert.equal(res.body[0].title, 'Jaws');
+          done();
+        }
+      );
+    });
+
+  });
 });
