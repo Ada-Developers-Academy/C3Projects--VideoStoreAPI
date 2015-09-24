450d40303139a096d0c3994c266f7377045376aa
marleigh <camarleigh@gmail.com>
2015-09-24 14:38:41 -0700

    passing customer endpoint tests.

--- test/controllers/customers.js
+++ test/controllers/customers.js
@@ -28,8 +28,53 @@ describe("Endpoints for /customers", function() {
   })
 
 // '/customers'
+  describe("GET all customers", function () {
+    var customer_request;
 
+    beforeEach(function(done) {
+      customer_request = agent.get('/customers').set('Accept', 'application/json');
+      done();
+    })
+
+    it ("responds with json", function(done) {
+      customer_request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
+    })
+
+    it ("returns an array of customer objects", function(done) {
+      customer_request.expect(200, function(err,result) {
+        assert.equal(result.body.length, 5);
+
+        var keys = ['id', 'name', 'registered_at', 'address', 'city', 'state', 'postal_zip', 'phone_number', 'credit'];
+        assert.deepEqual(Object.keys(result.body[0]), keys);
+        done();
+      });
+    });
+  });
 // '/customers/:name/:results_per_page/:page_number'
+  describe ("Get a subset of customers", function() {
+    var customer_request;
+
+    it("can get subset of customers by name", function() {
+      customer_request = agent.get('/customers/:name/5/1').set('Accept', 'application/json');
+      customer_request
+          .expect('Content-Type', /application\/json/)
+          .expect(200, function(error,result) {
+            assert.equal(result.body.length, 5);
+
+            var expected_customers = ['Alice', 'Shanna', 'Marleigh', 'Joe', 'Steve'],
+            actual_customers = [];
+
+            for(var index in result.body) {
+              actual_customers.push(result.body[0].name);
+            }
+
+            assert.deepEqual(expected_customers, actual_customers);
+            done(error);
+          })
+    })
+  })
 // '/customers/:registered_at/:results_per_page/:page_number'
 // '/customers/:postal_code/:results_per_page/:page_number'
 // '/customers/:id/current'
