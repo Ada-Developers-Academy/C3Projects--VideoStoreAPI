48d4b9a69db6889c9c9d8e636f75e81762e46997
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-24 13:32:12 -0700

    Seperated customers/:id into two seperate API endpoints (customers/:id/current & customers/:id/history) and also sorted movie results by check_out_date.

--- controllers/customers.js
+++ controllers/customers.js
@@ -13,7 +13,6 @@ function sortBy(sort_by, req, res) {
 
 exports.customersController = {
   // GET /customers
-  // returns all customers
   index: function(req, res) {
     // var results = { "customers": [] }
     var db = new Customer();
@@ -22,24 +21,12 @@ exports.customersController = {
     });
   },
 
-  // GET /customers/:id
-  // returns currently checked out movies & rental history
-  show: function(req, res) {
-    var id = req["params"]["id"];
-    var db = new Customer();
-    db.find_checked_out(id, function(err, result) {
-      return res.status(200).json(result);
-    });
-  },
-
   // GET /customers/by_name?n=XXX&p=XXX
-  // returns customers sorted by name & only the number & pages selected
   showByName: function(req, res) {
     sortBy("name", req, res);
   },
 
   // GET /customers/by_registered_at?n=XXX&p=XXX
-  // returns customers sorted by registered at & only the number & pages selected
   // NOTE: Need to change registered at to a time object? Sorting alphabetally vs. by date!
   showByRegistered_at: function(req, res) {
     var number = req["query"]["n"];
@@ -66,8 +53,25 @@ exports.customersController = {
   },
 
   // GET /customers/by_postal_code?n=XXX&p=XXX
-  // returns customers sorted by postal code & only the number & pages selected
   showByPostalCode: function(req, res) {
     sortBy("postal_code", req, res);
+  },
+
+  // GET /customers/:id/current
+  showCustomerCurrent: function(req, res) {
+    var id = req["params"]["id"];
+    var db = new Customer();
+    db.find_current(id, function(err, result) {
+      return res.status(200).json(result);
+    });
+  },
+
+  // GET /customers/:id/history
+  showCustomerHistory: function(req, res) {
+    var id = req["params"]["id"];
+    var db = new Customer();
+    db.find_history(id, function(err, result) {
+      return res.status(200).json(result);
+    });
   }
 };
--- routes/customers.js
+++ routes/customers.js
@@ -2,26 +2,34 @@ var express = require('express');
 var router = express.Router();
 var customer_exports = require('../controllers/customers');
 
-/* GET users listing. */
+// all customers
 router.get('/', function(req, res, next) {
   return customer_exports.customersController.index(req, res);
 });
 
+// all customers by name (optional query for number (n=#) & page (p=#))
 router.get('/by_name', function(req, res, next) {
   return customer_exports.customersController.showByName(req, res);
 });
 
+// all customers by registered at (optional query for number (n=#) & page (p=#))
 router.get('/by_registered_at', function(req, res, next) {
   return customer_exports.customersController.showByRegistered_at(req, res);
 });
 
+// all customers by postal code (optional query for number (n=#) & page (p=#))
 router.get('/by_postal_code', function(req, res, next) {
   return customer_exports.customersController.showByPostalCode(req, res);
 });
 
-router.get('/:id', function(req, res, next) {
-  return customer_exports.customersController.show(req, res);
+// a single customer's currently checked out movies
+router.get('/:id/current', function(req, res, next) {
+  return customer_exports.customersController.showCustomerCurrent(req, res);
 });
 
+// a single customer's previously checked out (& returned) movies
+router.get('/:id/history', function(req, res, next) {
+  return customer_exports.customersController.showCustomerHistory(req, res);
+});
 
 module.exports = router;
