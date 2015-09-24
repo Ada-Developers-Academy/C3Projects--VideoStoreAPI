dc7790282a8cbc520d2efd775f27747454d22305
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-24 13:57:46 -0700

    Removed public-facing/default Express page.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var movie_exports = require('../controllers/movies')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 router.get('/zomg', function(req, res, next){
   var results = {
     zomg: "it works!"
