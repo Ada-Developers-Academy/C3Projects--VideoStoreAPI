48f0408a12b2c174f44241857535b2983ff2a794
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-24 23:25:23 -0700

    changed test for put /:customer_id to use customer_id in the params and the movie_title in the request body

--- test/controllers/rentals_test.js
+++ test/controllers/rentals_test.js
@@ -13,7 +13,7 @@ describe('/rentals', function() {
   describe("POST '/'", function() {
   });
 
-  describe("PUT '/Wait%20Until%20Dark'", function() {
+  describe("PUT '/2'", function() {
     var rental = new Rental;
     var request;
 
@@ -21,24 +21,35 @@ describe('/rentals', function() {
       var data = {
         rentals: [
           { checkout_date: '2015-09-16', return_date: '', movie_title: 'Wait Until Dark', customer_id: 9 },
+          { checkout_date: '2015-08-24', return_date: '', movie_title: 'Wait Until Dark', customer_id: 2 },
         ]
       }
       resetTables(data, done);
     });
 
     beforeEach(function(done) {
-      request = agent.put('/rentals/Wait%20Until%20Dark').set('Content-Type', 'application/json')
+      request = agent.put('/rentals/2').set('Content-Type', 'application/json')
       .send({'return_date':'2015-09-23'})
+      .send({'movie_title':'Wait Until Dark'})
       .end(done);
     });
 
     it('updates the rental to have a return_date', function(done) {
       request
-      rental.findBy('movie_title', 'Wait Until Dark', function(err, rows) {
+      rental.findBy('customer_id', 2, function(err, rows) {
         assert.equal(err, undefined);
+        assert.equal(rows.length, 1);
+        assert.equal(rows[0].customer_id, 2)
         assert.equal(rows[0].return_date, '2015-09-23');
+
+        rental.findBy('customer_id', 9, function(err, rows) {
+          assert.equal(err, undefined);
+          assert.equal(rows.length, 1);
+          assert.equal(rows[0].customer_id, 9);
+          assert.equal(rows[0].return_date, '');
+          done();
+        });
       });
-      done();
     });
   });
 });
