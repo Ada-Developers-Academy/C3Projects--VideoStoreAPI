eac24db7093c755fb8b3cf1bd66bf45b95e70582
Lindsey Reno <lindseyannereno@gmail.com>
2015-09-25 12:46:29 -0700

    added tests for new endpoint /movies/:title/customers with query options

--- test/controllers/movies_test.js
+++ test/controllers/movies_test.js
@@ -478,4 +478,202 @@ describe('/movies', function() {
       });
     });
   });
+
+  describe("GET '/:title/customers?status=current'", function() {
+    var request;
+
+    before(function(done) {
+      var data = {
+        movies: [
+          { title: 'Jaws and Maws', overview: 'Worm!', release_date: '2015-09-12', inventory: 11 },
+          { title: 'Jaws', overview: 'Shark!', release_date: '1975-06-19', inventory: 10 },
+          { title: 'The French Connection', overview: 'Bonjour!', release_date: '1971-10-07', inventory: 8 }
+        ],
+        customers: [
+          { name: 'Customer1', registered_at: '2015-01-02', address: 'Address1', city: 'City1', state: 'State1', postal_code: 'Zip1', phone: 'Phone1', account_balance: '1250' },
+          { name: 'Customer2', registered_at: '2014-12-01', address: 'Address2', city: 'City2', state: 'State2', postal_code: 'Zip2', phone: 'Phone2', account_balance: '1000' },
+          { name: 'Customer3', registered_at: '2014-01-25', address: 'Address3', city: 'City3', state: 'State3', postal_code: 'Zip3', phone: 'Phone3', account_balance: '3000' }
+        ],
+        rentals: [
+          { checkout_date: '2015-03-16', return_date: '2015-03-20', movie_title: 'North by Northwest', customer_id: 2 },
+          { checkout_date: '2015-09-09', return_date: '', movie_title: 'Wait Until Dark', customer_id: 3 },
+          { checkout_date: '2015-08-10', return_date: '', movie_title: 'Jaws', customer_id: 1 }
+        ]
+      }
+      resetTables(data, done);
+    });
+
+    beforeEach(function() {
+      request = agent.get('/movies/Jaws/customers?status=current').set('Accept', 'application/json');
+    });
+
+    it('responds with json', function(done) {
+      request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
+    });
+
+    it("returns the 1 customer that currently has 'Jaws' checked out", function(done){
+      request
+        .expect(200, function(err, res) {
+          assert.equal(err, undefined);
+          assert.equal(res.body.length, 1);
+          assert.equal(res.body[0].customers_id, 1);
+          assert.equal(res.body[0].name, 'Customer1');
+          assert.equal(res.body[0].rental_id, 3);
+          assert.equal(res.body[0].checkout_date, '2015-08-10');
+          done();
+        }
+      );
+    });
+  });
+
+  describe("GET '/:title/customers?status=past&sort=customer_id'", function() {
+    var request;
+
+    before(function(done) {
+      var data = {
+        movies: [
+          { title: 'Jaws and Maws', overview: 'Worm!', release_date: '2015-09-12', inventory: 11 },
+          { title: 'Jaws', overview: 'Shark!', release_date: '1975-06-19', inventory: 10 },
+          { title: 'The French Connection', overview: 'Bonjour!', release_date: '1971-10-07', inventory: 8 }
+        ],
+        customers: [
+          { name: 'Customer1', registered_at: '2015-01-02', address: 'Address1', city: 'City1', state: 'State1', postal_code: 'Zip1', phone: 'Phone1', account_balance: '1250' },
+          { name: 'Customer2', registered_at: '2014-12-01', address: 'Address2', city: 'City2', state: 'State2', postal_code: 'Zip2', phone: 'Phone2', account_balance: '1000' },
+          { name: 'Customer3', registered_at: '2014-01-25', address: 'Address3', city: 'City3', state: 'State3', postal_code: 'Zip3', phone: 'Phone3', account_balance: '3000' }
+        ],
+        rentals: [
+          { checkout_date: '2015-03-16', return_date: '2015-03-20', movie_title: 'North by Northwest', customer_id: 2 },
+          { checkout_date: '2015-09-09', return_date: '', movie_title: 'Wait Until Dark', customer_id: 3 },
+          { checkout_date: '2015-08-10', return_date: '', movie_title: 'Jaws', customer_id: 1 }
+        ]
+      }
+      resetTables(data, done);
+    });
+
+    beforeEach(function() {
+      request = agent.get('/movies/North%20by%20Northwest/customers?status=past&sort=customer_id').set('Accept', 'application/json');
+    });
+
+    it('responds with json', function(done) {
+      request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
+    });
+
+    it("returns the 1 customer that has checked out 'North by Northwest' in the past", function(done){
+      request
+        .expect(200, function(err, res) {
+          assert.equal(err, undefined);
+          assert.equal(res.body.length, 1);
+          assert.equal(res.body[0].customer_id, 2);
+          assert.equal(res.body[0].name, 'Customer2');
+          assert.equal(res.body[0].rental_id, 1);
+          assert.equal(res.body[0].checkout_date, '2015-03-16');
+          done();
+        }
+      );
+    });
+  });
+
+  describe("GET '/:title/customers?status=dog'", function() {
+    var request;
+
+    before(function(done) {
+      var data = {
+        movies: [
+          { title: 'Jaws and Maws', overview: 'Worm!', release_date: '2015-09-12', inventory: 11 },
+          { title: 'Jaws', overview: 'Shark!', release_date: '1975-06-19', inventory: 10 },
+          { title: 'The French Connection', overview: 'Bonjour!', release_date: '1971-10-07', inventory: 8 }
+        ],
+        customers: [
+          { name: 'Customer1', registered_at: '2015-01-02', address: 'Address1', city: 'City1', state: 'State1', postal_code: 'Zip1', phone: 'Phone1', account_balance: '1250' },
+          { name: 'Customer2', registered_at: '2014-12-01', address: 'Address2', city: 'City2', state: 'State2', postal_code: 'Zip2', phone: 'Phone2', account_balance: '1000' },
+          { name: 'Customer3', registered_at: '2014-01-25', address: 'Address3', city: 'City3', state: 'State3', postal_code: 'Zip3', phone: 'Phone3', account_balance: '3000' }
+        ],
+        rentals: [
+          { checkout_date: '2015-03-16', return_date: '2015-03-20', movie_title: 'North by Northwest', customer_id: 2 },
+          { checkout_date: '2015-09-09', return_date: '', movie_title: 'Wait Until Dark', customer_id: 3 },
+          { checkout_date: '2015-08-10', return_date: '', movie_title: 'Jaws', customer_id: 1 }
+        ]
+      }
+      resetTables(data, done);
+    });
+
+    beforeEach(function() {
+      request = agent.get('/movies/Jaws/customers?status=dog').set('Accept', 'application/json');
+    });
+
+    it('responds with json', function(done) {
+      request
+        .expect('Content-Type', /application\/json/)
+        .expect(400, done);
+    });
+
+    it("returns a status code of 400 and an error message of 'Bad request'", function(done){
+      request
+        .expect(400, function(err, res) {
+          assert.equal(err, undefined);
+          assert.equal(res.error.status, 400);
+          assert.equal(res.error.text, '"Bad request"');
+          done();
+        }
+      );
+    });
+  });
+
+  describe("GET '/:title/customers?status=past&sort=name'", function() {
+    var request;
+
+    before(function(done) {
+      var data = {
+        movies: [
+          { title: 'Wait Until Dark', overview: 'Worm!', release_date: '2015-09-12', inventory: 11 },
+          { title: 'Jaws', overview: 'Shark!', release_date: '1975-06-19', inventory: 10 },
+          { title: 'North by Northwest', overview: 'Bonjour!', release_date: '1971-10-07', inventory: 8 }
+        ],
+        customers: [
+          { name: 'Customer1', registered_at: '2015-01-02', address: 'Address1', city: 'City1', state: 'State1', postal_code: 'Zip1', phone: 'Phone1', account_balance: '1250' },
+          { name: 'Customer2', registered_at: '2014-12-01', address: 'Address2', city: 'City2', state: 'State2', postal_code: 'Zip2', phone: 'Phone2', account_balance: '1000' },
+          { name: 'Customer3', registered_at: '2014-01-25', address: 'Address3', city: 'City3', state: 'State3', postal_code: 'Zip3', phone: 'Phone3', account_balance: '3000' }
+        ],
+        rentals: [
+          { checkout_date: '2015-03-16', return_date: '2015-03-20', movie_title: 'North by Northwest', customer_id: 2 },
+          { checkout_date: '2015-02-20', return_date: '2015-02-23', movie_title: 'North by Northwest', customer_id: 1},
+          { checkout_date: '2015-09-09', return_date: '', movie_title: 'Wait Until Dark', customer_id: 3 },
+          { checkout_date: '2015-08-10', return_date: '', movie_title: 'Jaws', customer_id: 1 }
+        ]
+      }
+      resetTables(data, done);
+    });
+
+    beforeEach(function() {
+      request = agent.get('/movies/North%20by%20Northwest/customers?status=past&sort=name').set('Accept', 'application/json');
+    });
+
+    it('responds with json', function(done) {
+      request
+        .expect('Content-Type', /application\/json/)
+        .expect(200, done);
+    });
+
+    it("returns the 2 customers that have checked out 'North by Northwest' in the past sorted by name", function(done){
+      request
+        .expect(200, function(err, res) {
+          assert.equal(err, undefined);
+          assert.equal(res.body.length, 2);
+          assert.equal(res.body[0].customer_id, 1);
+          assert.equal(res.body[0].name, 'Customer1');
+          assert.equal(res.body[0].rental_id, 2);
+          assert.equal(res.body[0].checkout_date, '2015-02-20');
+          assert.equal(res.body[1].customer_id, 2);
+          assert.equal(res.body[1].name, 'Customer2');
+          assert.equal(res.body[1].rental_id, 1);
+          assert.equal(res.body[1].checkout_date, '2015-03-16');
+          done();
+        }
+      );
+    });
+  });
 });
